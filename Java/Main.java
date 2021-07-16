811d6b4ec7b7a22fa9c720e05b617a31b48dd92d
seungmin-park <78605779+seungmin-park@users.noreply.github.com>
2021-07-16 23:30:10 +0900

    graph bfs and dfs

--- Java/Main.java
+++ Java/Main.java
@@ -1,24 +1,53 @@
 package Java;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
+	static int n;
+	static int m;
+	static int[][] arr;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String[] s = br.readLine().split("");
-		Integer[] num = new Integer[s.length];
-		for(int i = 0; i < s.length; i++){
-			num[i] = Integer.parseInt(s[i]);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] place = br.readLine().split(" ");
+		n = Integer.parseInt(place[1]);
+		m = Integer.parseInt(place[0]);
+		arr = new int[n+1][m+1];
+		for(int i = 1; i <=m ; i++){
+			String[] item = br.readLine().split(" ");
+			int dX = Integer.parseInt(item[0]);
+			int dY = Integer.parseInt(item[1]);
+			arr[dX][dY] = 1;
 		}
-		Arrays.sort(num, Collections.reverseOrder());
-		for(int x : num){
-			bw.write(String.valueOf(x));
+
+		int cnt = 0;
+		for(int i = 0; i <n ; i++){
+			for(int j = 0; j < m; j++){
+				if(dfs(i,j)){
+					cnt++;
+				}
+			}
 		}
+		bw.write(String.valueOf(cnt));
 		bw.flush();
 		bw.close();
 		br.close();
 	}
+	private static boolean dfs(int x, int y) {
+		if(x<= -1 || x >= n || y<= -1 || y>=m){
+			return false;
+		}
+
+		if(arr[x][y] == 0){
+			arr[x][y] = 1;
+			dfs(x-1, y);
+			dfs(x, y-1);
+			dfs(x+1, y);
+			dfs(x, y+1);
+			return true;
+		}
+
+		return false;
+	}
+
 }
