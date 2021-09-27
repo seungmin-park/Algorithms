8dd2e3c5c0d221a4d071f04a53c75697e0dc0532
seungmin-park <tmddudals369@naver.com>
2021-09-28 00:24:28 +0900

    큰 수 더하기

--- Java/Main.java
+++ Java/Main.java
@@ -1,44 +1,17 @@
 package Java;
 
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] countAndRepeat = br.readLine().split(" ");
-		int repeat = Integer.parseInt(countAndRepeat[1]);
-		String[] status = br.readLine().split(" ");
-		for(int i = 0; i < repeat; i ++){
-			String[] command = br.readLine().split(" ");
-			if(command[0].equals("1")){
-				status[Integer.parseInt(command[1]) -1] = command[2];
-			}else if(command[0].equals("2")){
-				for(int j = Integer.parseInt(command[1]) - 1; j <= Integer.parseInt(command[2]) -1 ; j ++){
-					if(status[j].equals("1")){
-						status[j] = "0";
-					}else{
-						status[j] = "1";
-					}
-				}
-			}
-			else if(command[0].equals("3")){
-				for(int j = Integer.parseInt(command[1]) - 1; j <= Integer.parseInt(command[2]) -1 ; j ++){
-					status[j] = "0";
-					
-				}
-			}
-			else if(command[0].equals("4")){
-				for(int j = Integer.parseInt(command[1]) - 1; j <= Integer.parseInt(command[2]) -1 ; j ++){
-					status[j] = "1";
-					
-				}
-			}
-		}
-		for(String x : status){
-			bw.write(x);
-			bw.write(" ");
-		}
+		BigInteger numA = new BigInteger(countAndRepeat[0]);
+		BigInteger numB = new BigInteger(countAndRepeat[1]);
+
+		bw.write(String.valueOf(numA.add(numB)));
 		bw.flush();
 		bw.close();
 		br.close();
